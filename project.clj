c1a1baef0b76d54127edd0c1be13f33057acf36c
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-03 00:39:56 +0800

    vendor: Installed jdbc and pgsql

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "MIT"
             :url "https://opensource.org/license/mit/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [cheshire "5.11.0"]]
+                 [cheshire "5.11.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.postgresql/postgresql "42.4.0"]]
   :main ^:skip-aot clojureverse.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
