3112bc8c4ae236195e25019a9ac1fc823fad7515
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-03 00:02:51 +0800

    vendor: Installed cheshire

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://github.com/data-miner00/clojureverse"
   :license {:name "MIT"
             :url "https://opensource.org/license/mit/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cheshire "5.11.0"]]
   :main ^:skip-aot clojureverse.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
