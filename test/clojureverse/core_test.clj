5ac1ba2ec76ff9f1dec811741cc2d16157835b9f
data-miner00 <data-miner00@users.noreply.github.com>
2023-06-04 07:41:10 +0800

    test: Added cheshire stringify example

--- test/clojureverse/core_test.clj
+++ test/clojureverse/core_test.clj
@@ -1,7 +1,14 @@
 (ns clojureverse.core-test
   (:require [clojure.test :refer :all]
-            [clojureverse.core :refer :all]))
+            [clojureverse.core :refer :all]
+            [cheshire.core :as cheshire]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
+  (testing "I Success."
     (is (= 1 1))))
+
+;; Usage of cheshire/generate-string
+(deftest cheshire-generate-string-test
+  (testing "Stringify map with 2 properties"
+    (def person {:name "Sandra" :age 31})
+    (is (= (cheshire/generate-string person) "{\"name\":\"Sandra\",\"age\":31}"))))
